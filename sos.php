7992ac65393252db1f1211042bf332ceeb7e6931
author <author@lugeshop.com>
2026-06-02 16:13:29 +0800

    update sos.php

--- sos.php
+++ sos.php
@@ -1,5 +1,5 @@
 <?php
-$command = 'ssh linchingyen@125.228.205.164 "python E:\sch\sos.py"';
+$command = 'ssh linchingyen@125.228.205.164 "python E:\sch\sos.py" > /dev/null 2>&1 &';
 $output = shell_exec($command);
 echo $output;
 echo "<script>parent.document.getElementById('myframe').src = '';</script>";
